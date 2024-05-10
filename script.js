dafb969d73cc302ee4b588f7b0db1738da2ab84a
justZa7 <154692649+justZa7@users.noreply.github.com>
2024-05-10 10:58:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const navLinks = document.getElementById("nav-links");
+const menuBtn = document.getElementById("menu-btn");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-circle-line" : "ri-menu-4-line"
+  );
+});
+
+const playBtn = document.getElementById('play-btn');
+const audio = document.querySelector('audio');
+
+playBtn.addEventListener('click', function () {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+})
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-4-line");
+});
+
+// const scrollRevealOption = {
+//   distance: "50px",
+//   origin: "bottom",
+//   duration: 1000,
+// };
+
+// // header container
+// ScrollReveal().reveal(".header_content h1", {
+//   ...scrollRevealOption,
+// });
+
+// ScrollReveal().reveal(".header_content .section_description", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
+
+// ScrollReveal().reveal(".header_content .header_btn", {
+//   ...scrollRevealOption,
+//   delay: 1000,
+// });
+
+// // about container
+// ScrollReveal().reveal(".about_content .section_header", {
+//   ...scrollRevealOption,
+// });
+
+// ScrollReveal().reveal(".about_content .section_description", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
+
+// ScrollReveal().reveal(".about_content .about_btn", {
+//   ...scrollRevealOption,
+//   delay: 1000,
+// });
+
+// // service container
+// ScrollReveal().reveal(".service_card", {
+//   ...scrollRevealOption,
+//   interval: 500,
+// });
+
+// // portfolio container
+// ScrollReveal().reveal(".portfolio_card", {
+//   duration: 1000,
+//   interval: 500,
+// });
